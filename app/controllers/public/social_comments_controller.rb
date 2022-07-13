83b1da82e8d7be6a7f6e0eb257b9e84641444a9a
takashi41404 <tsukasamiyumegu@gmail.com>
2022-07-13 13:07:46 +0000

    comment

--- app/controllers/public/social_comments_controller.rb
+++ app/controllers/public/social_comments_controller.rb
@@ -1,10 +1,11 @@
-class Public::SocialCommentsController <
+class Public::SocialCommentsController < ApplicationController
 
   def create
     record = Record.find(params[:record_id])
     @comment = current_user.social_comments.new(social_comment_params)
     @comment.record_id = record.id
     @comment.save
+#    redirect_to record_path
   end
 
   def destroy
